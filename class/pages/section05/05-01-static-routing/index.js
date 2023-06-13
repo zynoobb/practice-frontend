177ab9d7c5f07d6ec2379834b6caf68bf5721dd8
zynoobb <117588482+zynoobb@users.noreply.github.com>
2023-06-14 00:36:47 +0900

    Feat : 05-01 page-routing 학습

---
+++ class/pages/section05/05-01-static-routing/index.js
@@ -0,0 +1,15 @@
+import { useRouter } from "next/router";
+
+export default function StaticRoutingPage() {
+  const router = useRouter();
+
+  const onClickMove = () => {
+    router.push("/section05/05-01-static-routing-moved");
+  };
+
+  return (
+    <div>
+      <button onClick={onClickMove}>페이지 이동하기</button>
+    </div>
+  );
+}
