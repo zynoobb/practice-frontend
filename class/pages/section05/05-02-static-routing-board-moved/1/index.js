6dfc9e1f03fd858ef9f6495d3a224c3d47cebaf6
zynoobb <117588482+zynoobb@users.noreply.github.com>
2023-06-14 01:16:21 +0900

    Feat : 05-02 page 라우팅 학습

---
+++ class/pages/section05/05-02-static-routing-board-moved/1/index.js
@@ -0,0 +1,3 @@
+export default function StaticRoutingMovedPage() {
+  return <div>1번 게시글로 페이지 이동이 완료되었습니다.</div>;
+}
---
+++ class/pages/section05/05-02-static-routing-board-moved/3/index.js
@@ -0,0 +1,3 @@
+export default function StaticRoutingMovedPage() {
+  return <div>3번 게시글로 페이지 이동이 완료되었습니다.</div>;
+}
