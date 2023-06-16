7b82d4adcd394a48d384d91bf79be87869d5d1d1
zynoobb <117588482+zynoobb@users.noreply.github.com>
2023-06-16 22:01:01 +0900

    Feat : 09-01 학습

---
+++ class/pages/section09/09-01-board-new/index.js
@@ -0,0 +1,13 @@
+export default function boardNewPage() {
+  // Fragment
+  return (
+    <>
+      <h1>등록페이지</h1>
+      제목 : <input type="text" />
+      <br />
+      내용: <input type="text" />
+      <br />
+      <button>등록하기</button>
+    </>
+  );
+}
